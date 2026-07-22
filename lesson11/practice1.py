e6f324ef945866d0516984777918eb54cc7f40cb
neverlightdarkvoid <30883921+neverlightdarkvoid@users.noreply.github.com>
2026-07-22 20:02:31 +0800

    [Add] lesson11

---
+++ lesson11/practice1.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright,Playwright,Browser,Page
+
+
+def crawl(p:Playwright) -> None:
+  browser:Browser = p.chromium.launch(headless=False)
+  page:Page = browser.new_page()
+
+  page.goto("https://zh.wikipedia.org")
+  page.get_by_placeholder("搜尋維基百科").first.fill("臺灣")
+  page.screenshot(path="lesson11/screenshot.png")
+  page.wait_for_timeout(10000)
+  browser.close()
+
+
+with sync_playwright() as p:
+  crawl(p)
